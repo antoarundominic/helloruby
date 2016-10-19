9bfc20af50d03a53a2970d8cdef6491c3fb32a17
Jagadeesh <jagadeesh.jkp@gmail.com>
2016-10-19 16:10:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-hello world
+<?PHP
+
+$no="9994285039";
+$apiToken="";  // PHPHive Truecaller API Token, Obtain it from https://tcapi.phphive.info/console/
+
+$uri = 'https://tcapi.phphive.info/'.$apiToken.'/search/'.$no;
+$ch = curl_init($uri);
+curl_setopt_array($ch, array(
+    CURLOPT_HTTPHEADER  => array('X-User: PHPHive'),
+    CURLOPT_RETURNTRANSFER  =>true,
+	CURLOPT_SSL_VERIFYHOST => false,
+	CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_VERBOSE     => 1
+));
+$response = curl_exec($ch);
+curl_close($ch);
+echo $response;
+
+?>
